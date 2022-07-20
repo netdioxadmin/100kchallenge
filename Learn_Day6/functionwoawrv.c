4c14fa519cb0fb57fd9153398082be411f03e801
netdioxadmini <netdioxadmini@gmail.com>
2022-07-20 12:19:42 +0000

    day6 function

---
+++ Learn_Day6/functionwoawrv.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int R;
+    R=sum();
+    printf("Result = %d",R);
+return 0;
+}
+int sum(){
+    int v1,v2,vr;
+    printf("Enter 2 values\n");
+    scanf ("%d%d",&v1,&v2);
+    vr=v1+v2;
+    return(vr);
+}
